607920371a45f7c7524d024cc2f2f36c767d396e
nickborbe <nickborbe@gmail.com>
2019-10-29 12:25:10 -0400

    wack a mole interval funciton making the mole appear and dissappear

--- week2/wack-a-mole/script.js
+++ week2/wack-a-mole/script.js
@@ -1,12 +1,22 @@
 window.onload = function(){
 
+    let score = 0;
+
     const theMole = document.createElement('div');
     theMole.id = 'mole';
+
+    const explosion = document.createElement('div');
+    explosion.id = 'explode'
+
+    let successfullyClicked = true;
+  
+
 
 
 
 
     setInterval(()=>{
+      
 
         let r = Math.random();
    
@@ -25,6 +35,10 @@ window.onload = function(){
             return;
         }
 
+        if(!successfullyClicked){
+            score -=2;
+        }
+
 
 
         let randomRow = Math.floor(Math.random() * 3) + 1;
@@ -34,9 +48,12 @@ window.onload = function(){
 
             document.querySelector(`.row${randomRow} > .col${randomColumn}`)
             .appendChild(theMole)
-        
-  
-    },500)
+            
+            successfullyClicked = false;
+
+
+            document.querySelector('h3 > span').innerText = score;
+    },1000)
 
 
 
@@ -46,9 +63,39 @@ window.onload = function(){
 
 
 
+   
 
+    let spots = document.getElementsByClassName('col');
 
+    for(let i= 0; i < spots.length; i++){
+        spots[i].onclick = function(e){
 
+            if(e.currentTarget.innerHTML == ""){
+                console.log('empty');
+                score -= 5;
+                successfullyClicked = false;
+            } else{
+
+                console.log('you got the mole');
+                score += 10;
+
+                e.currentTarget.innerHTML = "";
+                e.currentTarget.appendChild(explosion)
+                setTimeout(()=>{
+                    e.currentTarget.innerHTML = "";
+                },333)
+
+                successfullyClicked = true;
+            }
+
+            (score);
+
+            document.querySelector('h3 > span').innerText = score;
+
+           
+
+        }
+    }
 
 
 
