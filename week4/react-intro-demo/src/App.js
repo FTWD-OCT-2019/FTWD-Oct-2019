88df52ade52afd8ef25986770e4dba3e410da841
nickborbe <nickborbe@gmail.com>
2019-11-12 10:28:04 -0500

    rave or die code added

--- week4/react-intro-demo/src/App.js
+++ week4/react-intro-demo/src/App.js
@@ -11,6 +11,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       user: null,
+      raveBoxStyle: {backgroundColor: 'white'},
+      raveText: 'Rave',
     }
 
   }
@@ -34,6 +36,60 @@ class App extends React.Component {
     })
    }
 
+   rave = () =>{
+    let lightColors = [
+      "#f500d8",
+      '#c3ebcd',
+      "#a6feff",
+      "#ffd6fe",
+      "#ffdc82", 
+      "#fffcf5", 
+      "#e8feff",
+      "#abddff",
+      "#ff7373",
+      "#ff7070",
+      "#72ff70",
+      "#00f56e",
+      "#c400f5",
+    ]
+
+    let darkColors = [
+      "#78026a",
+      "#8c2020",
+      "#a8a007",
+      "black",
+      "#0e8f00",
+      "#009c91",
+    ]
+      let currentStyle = {...this.state.raveBoxStyle};
+
+      let randomLightColor = lightColors[Math.floor(Math.random() *lightColors.length)];
+      let randomDarkColor = darkColors[Math.floor(Math.random() *darkColors.length)];
+
+      let otherRandomThing = Math.random();
+
+      if(otherRandomThing > 0.5){
+        currentStyle.backgroundColor = randomDarkColor;
+        currentStyle.color = randomLightColor;
+      } else {
+        currentStyle.backgroundColor = randomLightColor;
+        currentStyle.color = randomDarkColor;
+      }
+
+      let key = {
+        "Rave": 'Or',
+        "Or": 'Die!',
+        "Die!": 'Rave'
+      }
+
+      let newWord = key[this.state.raveText];
+      
+      this.setState({
+        raveBoxStyle: currentStyle,
+        raveText: newWord
+      })
+   }
+
    
 
 
@@ -44,11 +100,12 @@ class App extends React.Component {
           return (
             <div>
 
-              <button id="grad1"> Rave Time!</button>
+              <button id="grad1"
+              onClick = {this.rave}
+              > Rave Time!</button>
 
               <div className="ravebox" style={this.state.raveBoxStyle}>
-
-                Rave
+                {this.state.raveText}
               </div>
 
               <button onClick = {this.logMeIn}>Login</button>
