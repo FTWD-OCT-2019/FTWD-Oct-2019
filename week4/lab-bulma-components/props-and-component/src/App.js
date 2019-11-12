a1b9a74dc458e5af400ba347e06989cda7ec21f8
nickborbe <nickborbe@gmail.com>
2019-11-12 10:06:43 -0500

    beginning of bulma

---
+++ week4/lab-bulma-components/props-and-component/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    
+    <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+
+
+    <button className="button is-success">This is a button</button>
+
+    </div>
+  );
+}
+
+export default App;
